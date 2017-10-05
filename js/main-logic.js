5bdaf68edd66e132397613a3d3582b10221013b8
smallpi <smallpi.yuewen@gmail.com>
2017-10-05 17:31:32 +0800

    debugging

--- js/main-logic.js
+++ js/main-logic.js
@@ -1,15 +1,18 @@
 /**
  * 菜单的开关逻辑
  */
-var menuSwitch = document.getElementById("menu-switch");
+var menuSwitchContainer = document.getElementById("menu-switch-container");
+var menuSwitchIcon = document.getElementById("menu-switch-icon");
 var nav = document.getElementById("nav");
 var isMenuShow = false;
-menuSwitch.addEventListener("click",function(){
+menuSwitchContainer.addEventListener("click",function(){
     if(isMenuShow){
         nav.style.display = "none";
+        menuSwitchContainer.style.backgroundColor = "#2980b9";
         isMenuShow = false;
     }else{
         nav.style.display = "block";
+        menuSwitchContainer.style.backgroundColor = "#404040";
         isMenuShow = true;
     }
 });
